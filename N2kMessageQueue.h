8fdd2cadc79832f7dcbef2a000d671ffccb735ac
Paul Reeve <preeve@pdjr.eu>
2022-12-12 09:16:09 +0100

    First upload

---
+++ N2kMessageQueue.h
@@ -0,0 +1,6 @@
+/**********************************************************************
+ * N2kMessageQueue - ADT implementing an N2K message transmit queue.
+ * Copyright (C) 2022 Paul Reeve <pdjr@pdjr.eu>
+ */
+
+ 
